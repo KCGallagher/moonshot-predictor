34dc081070f5edb1c186721f79de68316d4af5f3
KCGallagher <christopher.gallagher@seh.ox.ac.uk>
2022-01-25 12:51:29 +0000

    Read csv to database

--- database.py
+++ database.py
@@ -1,20 +1,39 @@
 # Creates a relational database from the binding data
 
+import pandas as pd
 import sqlite3
 from sqlite3 import Error
 
 def create_connection(db_file):
-    """ create a database connection to a SQLite database """
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+    return conn
 
+
+def main():
+    input_data = "data/activity_data.csv"
+    database = "sqlite/db/activity.db"
+
+    # Read in data
+    df = pd.read_csv(input_data)
+
+    # Create a database connection
+    conn = create_connection(database)
+
+    # Read csv into database
+    with conn:
+        df.to_sql('activity', conn, if_exists='append', index=False)
+
+    # Close connection if still open (fail-safe)
+    conn.close
 
 if __name__ == '__main__':
-    create_connection("sqlite/db/pythonsqlite.db")
+    main()
