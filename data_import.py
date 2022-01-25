19986fe005daf32894c106c3c198748a197ad516
Kit Gallagher <gallagherkit@outlook.com>
2022-01-25 19:20:28 +0000

    Split import dataframe

--- data_import.py
+++ data_import.py
@@ -11,14 +11,39 @@ def import_csv(filename):
     """
     return pd.read_csv(filename, sep=',')
 
+def record_assays(df):
+    """Removes SMILES data to create assay dataframe
+    
+    :param df: Dataframe of input data
+    :type df: Pandas Dataframe
+    :return: Dataframe of assay data
+    """
+    #Still need to sort this data
+    return df.drop(['SMILES'], axis = 1)
+
+def record_compounds(df):
+    """Isolate SMILES data to create compound dataframe
+    
+    :param df: Dataframe of input data
+    :type df: Pandas Dataframe
+    :return: Dataframe of assay data
+    """
+    return df['SMILES']
+
 
 def main():
     input_data = "data/activity_data.csv"
 
     # Read in data
     df = import_csv(input_data)
-    print(df.head())
 
+    df_assay = record_assays(df)
+    df_compounds = record_compounds(df)
+    
+    print(df_assay.head())
+    print(df_compounds.head())
+
+    # Still need to implement foreign key
 
 if __name__ == '__main__':
     main()
